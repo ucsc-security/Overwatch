15b0ecda7ccc33ecc7aa588b16dc97f730a66636
daniel <106453412+0xfbad@users.noreply.github.com>
2023-04-18 12:33:55 -0700

    Refactor + add solver count to solver message

--- jobs/iCTF-Solve-Announce.js
+++ jobs/iCTF-Solve-Announce.js
@@ -4,12 +4,20 @@ import Database from 'better-sqlite3';
 import { MessageFlags } from 'discord.js';
 
 const API_BASE_URL = 'https://imaginaryctf.org/api/solves/byuserid/';
-const db = new Database('iCTF.db');
-db.exec("CREATE TABLE IF NOT EXISTS solves_announced ('UserID' text, 'ChallengeID' text, PRIMARY KEY('UserID', 'ChallengeID'))");
+const API_CHALLENGES_URL = 'https://imaginaryctf.org/api/challenges/released';
 
-const getChallengesSolved = async (id) => {
+const db = new Database('iCTF.db');
+db.exec(`
+	CREATE TABLE IF NOT EXISTS solves_announced (
+		'UserID' text, 
+		'ChallengeID' text, 
+		PRIMARY KEY('UserID', 'ChallengeID')
+	)
+`);
+
+const getJsonFromUrl = async (url) => {
 	try {
-		const response = await fetch(`${API_BASE_URL}${id}`);
+		const response = await fetch(url);
 		const data = await response.json();
 
 		if (response.status === 200)
@@ -17,28 +25,56 @@ const getChallengesSolved = async (id) => {
 		else
 			return null;
 	} catch (error) {
-		console.error(`Error fetching challenge data: ${error}`);
+		console.error(`Error fetching data from "${url}": ${error}`);
 		return null;
 	}
+}
+
+const getChallengesSolved = async (id) => {
+	const url = `${API_BASE_URL}${id}`;
+	return await getJsonFromUrl(url);
+}
+
+const getChallenges = async () => {
+	return await getJsonFromUrl(API_CHALLENGES_URL);
 }
 
 const announceChallengeSolved = async (client, user, challenge) => {
 	const threadID = db.prepare('SELECT ThreadID FROM challenge_threads WHERE ChallengeID = ?').pluck().get(challenge.id);
 
-	if (threadID) {
-		const thread = await client.channels.fetch(threadID);
-		if (thread) {
-			await thread.send({ 
-				content: `<@${user.discordUserID}> has solved this challenge! 🎉`, 
-				flags: MessageFlags.SuppressNotifications 
-			});
-			db.prepare('INSERT INTO solves_announced (UserID, ChallengeID) VALUES (?, ?)').run(user.iCTFAccountID, challenge.id);
-
-			console.log(`iCTF-Announce: Announced <@${user.discordUserID}> (${user.iCTFAccountID}) solved ${challenge.id} - "${challenge.title}" in <#${threadID}>`);
-		} else {
-			console.error(`Failed to get thread for ${challenge.title} - ${challenge.id}`);
-		}
+	if (!threadID) {
+		console.error(`No thread for ""${challenge.title}"" - ${challenge.id}`);
+		return;
 	}
+
+	const thread = await client.channels.fetch(threadID);
+
+	if (!thread) {
+		console.error(`Failed to get thread for "${challenge.title}" - ${challenge.id}`);
+		return;
+	}
+
+	const releasedChallenges = await getChallenges();
+	const releasedChall = releasedChallenges.find(chall => chall.id === challenge.id);
+	const solvesCount = releasedChall ? releasedChall.solves_count : 'an unknown number of';
+
+	const content = `<@${user.discordUserID}> has solved this challenge! There are now ${solvesCount} solvers. 🎉`;
+	const msg = await thread.send({ content, flags: MessageFlags.SuppressNotifications });
+	db.prepare('INSERT INTO solves_announced (UserID, ChallengeID) VALUES (?, ?)').run(user.iCTFAccountID, challenge.id);
+
+	switch (solvesCount) {
+		case 1:
+			msg.react('🩸');
+			break;
+		case 2:
+			msg.react('🥈');
+			break;
+		case 3:
+			msg.react('🥉');
+			break;
+	}
+
+	console.log(`iCTF-Announce: Announced <@${user.discordUserID}> (${user.iCTFAccountID}) solved ${challenge.id} - "${challenge.title}" (${solvesCount} solvers) in <#${threadID}>`);
 }
 
 const findNewSolvers = async (client) => {
@@ -47,19 +83,20 @@ const findNewSolvers = async (client) => {
 	for (const user of users) {
 		const solvedChallenges = await getChallengesSolved(user.iCTFAccountID);
 
-		if (solvedChallenges) {
-			for (const solvedChallenge of solvedChallenges) {
-				const challengeID = solvedChallenge.challenge.id;
-				const isAnnounced = db.prepare('SELECT 1 FROM solves_announced WHERE UserID = ? AND ChallengeID = ?').pluck().get(user.iCTFAccountID, challengeID);
+		if (!solvedChallenges)
+			continue;
 
-				if (!isAnnounced) {
-					const challenge = {
-						id: challengeID,
-						title: solvedChallenge.challenge.title
-					};
+		for (const solvedChallenge of solvedChallenges) {
+			const challengeID = solvedChallenge.challenge.id;
+			const isAnnounced = db.prepare('SELECT 1 FROM solves_announced WHERE UserID = ? AND ChallengeID = ?').pluck().get(user.iCTFAccountID, challengeID);
 
-					await announceChallengeSolved(client, user, challenge);
-				}
+			if (!isAnnounced) {
+				const challenge = {
+					id: challengeID,
+					title: solvedChallenge.challenge.title
+				};
+
+				await announceChallengeSolved(client, user, challenge);
 			}
 		}
 	}

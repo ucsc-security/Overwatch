fe31f7671d9e7e31fca411f8d73db24b5c20dc2f
daniel <106453412+0xfbad@users.noreply.github.com>
2024-06-01 02:33:35 -0700

    New status

--- events/ready.js
+++ events/ready.js
@@ -4,11 +4,51 @@ import { loadCommands, loadJobs } from '#util/botStartup.js';
 const once = true;
 const eventType = Events.ClientReady;
 
+const ACTIVITIES = [
+	'over Slug Security',
+	'incoming connections',
+	'outgoing connections',
+	'network traffic',
+	'/var/log/auth.log',
+	'user logins',
+	'firewall logs',
+	'active connections',
+	'error logs',
+	'for configuration changes',
+	'for threat reports',
+	'system updates',
+	'intrusion attempts',
+	'malware signatures',
+	'DNS queries',
+	'honeypot activity',
+	'SIEM alerts',
+	'for port scans',
+	'web server logs',
+	'database logs',
+	'for suspicious IPs',
+	'for data exfiltration',
+	'for security patches',
+	'for privilege escalation'
+];
+
+async function updatePresence(client) {
+	const setPresence = () => {
+		const activity = ACTIVITIES[Math.floor(Math.random() * ACTIVITIES.length)]
+		client.user.setPresence({
+			activities: [{ name: activity, type: ActivityType.Watching }],
+			status: 'online'
+		});
+	};
+
+	setPresence();
+	setInterval(setPresence, 30_000);
+}
+
 async function invoke(client) {
 	await loadCommands(client);
 	await loadJobs(client);
 
-	client.user.setPresence({ activities: [{ name: `over Slug Security`, type: ActivityType.Watching }], status: 'online' });
+	await updatePresence(client);
 	console.log(`\nLogged in as ${client.user.tag}!`);
 }
 
